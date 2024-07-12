106a563b7f7fa6a3bf55156e0f1a4b0d9ab78550
Widewingz <henrylee1998@gmail.com>
2024-07-12 16:04:37 +1000

    Near complete

--- Search API/app.js
+++ Search API/app.js
@@ -1,5 +1,22 @@
 const form = document.querySelector('#searchForm')
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', async function (e) {
+    // addEvent listener to the form and listen for submit
     e.preventDefault();
-    
+    //Prevent defaults
+    const searchTerm = form.elements.query.value
+    //Access the input from the form 
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+
+    makeImages(res.data)
 })
+
+const makeImages = (shows) => {
+    // create a function to create element and append images
+    for (let result of shows) {
+        if (result.show.img) {
+            const img = document.createElement('IMG')
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }
+}
