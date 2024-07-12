24d7cb5fc1dd73b68a86d9c086bae558435aca80
Widewingz <henrylee1998@gmail.com>
2024-07-12 16:29:05 +1000

    Update app.js

--- Search API/app.js
+++ Search API/app.js
@@ -5,15 +5,22 @@ form.addEventListener('submit', async function (e) {
     //Prevent defaults
     const searchTerm = form.elements.query.value
     //Access the input from the form 
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
-
+    const config = { params: { q: searchTerm } }
+    //params config help us change the param of axios request
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config)
     makeImages(res.data)
+    form.elements.query.value = ''
+    //Emptying the value
+
 })
 
 const makeImages = (shows) => {
     // create a function to create element and append images
+    const existingImages = document.querySelectorAll('img');
+    existingImages.forEach(img => img.remove()); // Remove all existing images
+
     for (let result of shows) {
-        if (result.show.img) {
+        if (result.show.image) {
             const img = document.createElement('IMG')
             img.src = result.show.image.medium;
             document.body.append(img)
